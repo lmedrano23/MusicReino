8b1a8de7d6065b3595d7fd03d798b894f03d8c22
unknown <luis_medrano@uri.edu>
2026-04-13 01:51:08 -0400

    update #2: systematically ready. Working on Audio feedback next.

--- music_player.py
+++ music_player.py
@@ -4,12 +4,48 @@ import os
 
 play_list = r"./The_Playlist/"
 
+#if false then true if true then false and play/pause respectively
+play_button_state = False
 
-def nextTrack():
-    with os.scandir(play_list) as entries:
+#playlist array:
+tracklist = []
+#track names array:
+trackNames = []
+#track index:
+trackIdx = 0
+
+with os.scandir(play_list) as entries:
         for entry in entries:
             if entry.is_file():
-                print(entry.name)
+                tracklist.append(entry.path)
+                trackNames.append(entry.name)
+
+
+def nextTrack():
+    global trackIdx
+    trackIdx += 1
+    if not (trackIdx == len(tracklist)):
+        print(trackNames[trackIdx])
+    else:
+        print("Limit reached.")
+
+def prevTrack():
+    global trackIdx
+    trackIdx -= 1
+    if not (trackIdx < 0):
+        print(trackNames[trackIdx])
+    else:
+        print("Limit reached.")
+
+def playPause():
+    global play_button_state
+    if play_button_state == True:
+        print(play_button_state)
+        play_button_state = False
+    elif play_button_state == False:
+        print(play_button_state)
+        play_button_state = True
+    #print(play_button_state)
 
 root = Tk()
 frm = ttk.Frame(root)
@@ -21,12 +57,12 @@ frm.master.title("Reino Music Player")
 exitButton = ttk.Button(root, text="Quit", command=root.destroy)
 exitButton.place(relx=1.0, rely=1.0, anchor="se")
 
-prevButton = ttk.Button(root, text="<< Previous", command=nextTrack)
+prevButton = ttk.Button(root, text="<< Previous", command=prevTrack)
 prevButton.place(relx=0.5, rely=0.5, anchor="center")
 
-nextButton = ttk.Button(root, text="Next >>", command=root.destroy)
+nextButton = ttk.Button(root, text="Next >>", command=nextTrack)
 nextButton.place(relx=0.6, rely=0.5, anchor="center")
 
-ppButton = ttk.Button(root, text="Play/Pause", command=root.destroy)
+ppButton = ttk.Button(root, text="Play/Pause", command=playPause)
 ppButton.place(relx=0.4, rely=0.5, anchor="center")
 root.mainloop()
